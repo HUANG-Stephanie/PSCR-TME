072e44f8fe0df4c13290565f88119aa1eee4849a
HUANG-Stephanie <115583805+HUANG-Stephanie@users.noreply.github.com>
2023-10-23 09:39:43 +0200

    Add files via upload

---
+++ TME5/src/Pool.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Queue.h"
+#include "Job.h"
+#include <vector>
+#include <thread>
+
+namespace pr {
+
+class Pool {
+	Queue<Job> queue;
+	std::vector<std::thread> threads;
+public:
+	Pool(int qsize) : queue(qsize){}
+
+	void start (int nbthread){
+		threads.reserve(nbthread);
+		for(int i = 0; i<nbthread;i++){
+			threads.emplace_back(&Pool::poolWorker, this);
+    	}
+	}
+
+	void submit (Job * job){
+		queue.push(job);
+	} 
+
+	void stop(){
+		queue.setBlocking(false);
+		for(auto & t: threads){
+			t.join();
+		}
+    	threads.clear();
+	}
+
+	~Pool(){ stop();}
+
+	void poolWorker(){
+    	while(true){
+			Job*j = queue.pop();
+			if(j == nullptr){
+				break;
+			}
+			j->run();
+			delete j;
+			}
+		}
+	};
+}
